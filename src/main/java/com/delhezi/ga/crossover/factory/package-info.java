1370d291a39ee9cdf44324fdcf0fad5a2a60ccec
Wojciech Wolszczak <wojciech.wolszczak@delhezi.com>
2015-08-13 18:28:42 +0200

    ok

--- src/main/java/com/delhezi/ga/crossover/factory/package-info.java
+++ src/main/java/com/delhezi/ga/crossover/factory/package-info.java
@@ -1,15 +1,6 @@
-/**
- * @(#)package-info.java
- * Copyright (C) 2008-2011 delhezi.com
- *
- * This class is released under the:
- * GNU Lesser General Public License (LGPL) version 3 or later.
- * http://www.gnu.org/copyleft/lesser.html
- */
-package com.delhezi.ga.crossover.factory;
-
+// CHECKSTYLE_OFF: RegexpHeader
 /**
  * Fabryka operatorów krzyżowania.
  * @since 1.0
  */
-
+package com.delhezi.ga.crossover.factory;
