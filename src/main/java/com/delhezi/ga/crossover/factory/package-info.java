d5fa5c5d749ca301aff5ca5588714d251d913bbb
Wojciech Wolszczak <wojciech.wolszczak@delhezi.com>
2015-08-13 19:40:31 +0200

    ok

--- src/main/java/com/delhezi/ga/crossover/factory/package-info.java
+++ src/main/java/com/delhezi/ga/crossover/factory/package-info.java
@@ -4,12 +4,3 @@
  * @since 1.0
  */
 package com.delhezi.ga.crossover.factory;
-
-/*
- * Copyright (C) 2008-2011 delhezi.com
- *
- * This class is released under the:
- * GNU Lesser General Public License (LGPL) version 3 or later.
- * http://www.gnu.org/copyleft/lesser.html
- */
- 
